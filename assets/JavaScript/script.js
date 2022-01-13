1bb0a6f617c13a8d664e8c0dc72fc8007c1b3bde
Pedro Bigas <91469236+PedroBigas@users.noreply.github.com>
2022-01-13 12:53:14 -0300

    quase pronto

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -10,6 +10,10 @@ let musica = document.getElementById("musica");
 let srcMsc = document.getElementsByTagName("source")[0];
 let srcFoto = document.getElementsByTagName("img")[0];
 
+let TimingMusic = document.getElementById("barraTempo");
+let tempoatualMusica = document.getElementById("durante");
+let durationTotal = document.getElementById("duracao");
+
 let iconPlay = document.getElementsByTagName("ion-icon")[1];
 
 let IndexMsc = 0;
@@ -47,6 +51,17 @@ let musicas = [
     },
 
 ];
+
+onload = function(){
+    durationTotal.innerHTML = formatacao(Math.round(musica.duration));
+}
+
+musica.onloadeddata = function(){
+    TimingMusic.setAttribute("max",Math.round(musica.duration));
+    durationTotal.innerHTML = formatacao(Math.round(musica.duration));
+
+}
+
     function toggle() {
         //<ion-icon name="pause-outline"></ion-icon>
         iconPlay.setAttribute("name","pause-outline");
@@ -54,6 +69,8 @@ let musicas = [
 }
     function pause() {
         musica.pause();
+        mudarAtt(iconPlay,"name","play-outline");
+        mudarAtt(iconPlay,"onclick","play()");
 }
 
 
@@ -65,6 +82,37 @@ let musicas = [
         musica.play();
         attDados();
         mudarAtt(iconPlay,"name","pause-outline");
+        mudarAtt(iconPlay,"onclick","pause()");
+
+            setInterval(() => {
+                TimingMusic.value = Math.round(musica.currentTime);
+                tempoatualMusica.innerHTML = formatacao(Math.round(musica.currentTime));
+
+                    if (formatacao(Math.round(musica.currentTime)) == formatacao(Math.round(musica.duration))){
+                        prox();
+                    }
+
+
+            }, 1000);
+
+
+
+
+
+}
+    function voltar() {
+
+        musica.load();
+        IndexMsc -= 1;
+
+        
+            if(IndexMsc < 0){
+                IndexMsc = 0;
+        }
+
+
+        TimingMusic.value = 0
+        play();
 }
 
     function prox() {
@@ -77,11 +125,8 @@ let musicas = [
                 IndexMsc = 0;
         }
 
-        
-        srcMsc.setAttribute('src', musicas[IndexMsc].srcAudio);
-        srcFoto.setAttribute('src', musicas[IndexMsc].srcImg);
-
-        
+    
+        TimingMusic.value = 0
         play();
 }
 
@@ -91,5 +136,28 @@ let musicas = [
         titleMusic.innerHTML = musicas[IndexMsc].title;
         artistaMusic.innerHTML = "";
         artistaMusic.innerHTML = musicas[IndexMsc].artist;
-
+        srcMsc.setAttribute('src', musicas[IndexMsc].srcAudio);
+        srcFoto.setAttribute('src', musicas[IndexMsc].srcImg);
 }
+
+    function SetTiming() {
+        musica.currentTime = TimingMusic.value;
+            if(musica.paused == false){
+                musica.play();
+            }
+        musica.play();
+}
+
+    function formatacao(secs, format) {
+        let hr = Math.floor(secs / 3600);
+        let min = Math.floor((secs - (hr * 3600))/60);
+        let sec = Math.floor(secs - (hr * 3600) - (min *60));
+
+            if (min < 10){
+                min = "0" + min;
+            }
+            if (sec < 10){
+                sec = "0" + sec;
+            }
+            return min + ":" + sec;
+    }
